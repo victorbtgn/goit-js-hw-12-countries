890466549b21a586992a518a5ac7758f79c9bbef
Victor Batigin <bixs85@gmail.com>
2020-04-15 18:01:38 +0300

    add input with style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.css';
+import fetchCountries from './js/fetchCountries';
+
+fetchCountries();
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+export default function fetchCountries(searchQuery) {
+  return new Promise((resolve, reject) => {
+    resolve(console.log(`Hello)`));
+  });
+}
